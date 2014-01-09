9f640166c4512432ab19abe897a2e051602a4dfc
stenman <roger.stenman@gmail.com>
2014-01-09 21:27:23 +0100

    -Implemented a more sofisticated way of connecting to the target URL to avoid user agent sniffing

--- src/main/java/com/example/parseurlsource/parser/JsoupUrlParser.java
+++ src/main/java/com/example/parseurlsource/parser/JsoupUrlParser.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.jsoup.Connection.Response;
 import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -20,7 +21,10 @@ public class JsoupUrlParser extends UrlParser {
 
 	private void setDoc() throws HttpStatusException, IOException {
 		doc = null;
-			doc = Jsoup.connect(url).get();
+		Response response = Jsoup.connect(url).ignoreContentType(true)
+				.userAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0").referrer("http://www.google.com")
+				.timeout(12000).followRedirects(true).execute();
+		doc = response.parse();
 	}
 
 	public String getTitle() throws HttpStatusException, IOException {
